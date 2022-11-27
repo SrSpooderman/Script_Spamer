f407fe7328bbb2cfbca8667c35ccf6c530019829
Francisco Valencia <77443507+SrSpooderman@users.noreply.github.com>
2022-11-27 13:32:37 +0100

    Mejora de configuracion palabra

--- Palabra.py
+++ Palabra.py
@@ -1,11 +1,13 @@
+#importaciones
 import pyautogui
 import time
-xdd = 1
-time.sleep(5)
-while xdd == 1:
-    pyautogui.typewrite("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-    time.sleep(1)
-    pyautogui.press("enter")
-    pyautogui.typewrite("aaaaaaaaaaaaaaaaaaaaaaaaaa")
-    time.sleep(1)
+#variables
+array_repetir = str(input("inserte palabra para repetir: "))
+tiempo_antes_iniciar = int(input("Inserte tiempo antes de iniciar: "))
+tiempo_entre__envios = int(input("Inserte tiempo entre mensaje y mensaje: "))
+#main
+time.sleep(tiempo_antes_iniciar)
+while True:
+    pyautogui.typewrite(array_repetir)
+    time.sleep(tiempo_entre__envios)
     pyautogui.press("enter")
